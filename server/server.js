ee885758e6a6d03e5fa8ad018da8a4980d86b67b
lcchai-sg <lc.chai.sg@gmail.com>
2021-05-31 17:55:46 +0800

    working socket io connect/disconnect

--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 const port = process.env.PORT || 3000;
 
 const publicPath = path.join(__dirname + '/../public');
-const app = express();
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => { console.log(`Server is up and listening on port ${port}`) });
+io.on('connection', (socket) => {
+    console.log('New user connected!');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected!');
+    })
+})
+
+
+server.listen(port, () => { console.log(`Server is up and listening on port ${port}`) });
